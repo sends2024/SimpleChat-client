f1e90ce620fa133eaa1c3b459ae05be68a133ddf
y <2687545259@qq.com>
2025-11-27 15:38:56 +0800

    feat: 调整组件目录结构，路由增加layout包裹

--- renderer/src/router/index.tsx
+++ renderer/src/router/index.tsx
@@ -2,22 +2,33 @@ import HomePage from '@/pages/HomePage'
 import AuthPage from '@/pages/AuthPage'
 
 import { createHashRouter, Navigate } from 'react-router'
+import { Children } from 'react'
+import { CommonLayout } from '@/pages/CommonLayout'
 
 const route = [
     {
         name: 'index',
         path: '/',
-        Component: () => <Navigate to="/home" replace />
-    },
-    {
-        name: 'home',
-        path: '/home',
-        Component: HomePage
+        Component: () => <Navigate to="/layout" replace />
     },
     {
         name: 'auth',
         path: '/auth',
         Component: AuthPage
+    },
+    {
+        path: '/layout',
+        Component: CommonLayout,
+        children: [
+            {
+                index: true,
+                Component: HomePage
+            },
+            {
+                path: 'home',
+                Component: HomePage
+            }
+        ]
     }
 ]
 const router = createHashRouter(route)
